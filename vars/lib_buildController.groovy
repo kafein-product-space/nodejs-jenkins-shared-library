519d6dd1174a655a9e7a3df42e101a0e4c30930b
Ferhat Kacar <ferhatkacar.partner@kafein.com.tr>
2024-08-06 12:28:57 +0300

    Updated build

--- vars/lib_buildController.groovy
+++ vars/lib_buildController.groovy
@@ -1,15 +1,23 @@
 def call(Map config) {
-    config.b_config.projects.each { it ->
+    config.b_config.containerConfig.each { it ->
+
         def builder = "npm"
 
-        if ( config.b_config.project.builderVersion != "nodejs" ) {
+        if (config.b_config.project.builderVersion != "nodejs") {
             builder = "${tool config.b_config.project.builderVersion}/bin/npm"
         }
 
-        sh """
-        cd ${it.path} && \
-        ${builder} install && \
-        ${builder} run ${config.b_config.project.buildCommand ? config.b_config.project.buildCommand : 'build'}
-        """
+        if (it.containsKey("path")) {
+            sh """
+            cd ${it.path} && \
+            ${builder} install && \
+            ${builder} run ${config.b_config.project.buildCommand ? config.b_config.project.buildCommand : 'build'}
+            """
+        } else {
+            sh """
+            ${builder} install && \
+            ${builder} run ${config.b_config.project.buildCommand ? config.b_config.project.buildCommand : 'build'}
+            """
+        }
     }
 }
