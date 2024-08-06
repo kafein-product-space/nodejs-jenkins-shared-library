29edaac466736ad77b4056f533fb96516d64bf0b
Ferhat Kacar <ferhatkacar.partner@kafein.com.tr>
2024-08-06 12:01:20 +0300

    Updated if checks

--- vars/lib_buildController.groovy
+++ vars/lib_buildController.groovy
@@ -1,12 +1,20 @@
 def call(Map config) {
-    def builder = "npm"
+    config.b_config.projects.each { it ->
+        def builder = "npm"
 
-    if ( config.b_config.project.builderVersion != "nodejs" ) {
-        builder = "${tool config.b_config.project.builderVersion}/bin/npm"
+        if ( config.b_config.project.builderVersion != "nodejs" ) {
+            builder = "${tool config.b_config.project.builderVersion}/bin/npm"
+        }
+
+        if ( it.containsKey("path") ) {
+            sh """
+            echo ${it.path}
+            cd ${it.path}
+            """
+        }
+        sh """
+        ${builder} install && \
+        ${builder} run ${config.b_config.project.buildCommand ? config.b_config.project.buildCommand : 'build'}
+        """
     }
-
-    sh """
-    ${builder} install && \
-    ${builder} run ${config.b_config.project.buildCommand ? config.b_config.project.buildCommand : 'build'}
-    """
 }
