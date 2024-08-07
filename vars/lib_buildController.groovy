c616b42a50ce8fa2ce0325b92be3dc21b46e9391
Ferhat Kacar <ferhatkacar.partner@kafein.com.tr>
2024-08-07 11:31:42 +0300

    Updated build controller

--- vars/lib_buildController.groovy
+++ vars/lib_buildController.groovy
@@ -5,17 +5,8 @@ def call(Map config) {
         builder = "${tool config.b_config.project.builderVersion}/bin/npm"
     }
 
-    config.b_config.project.each { it ->
-        def path = "."
-
-        if (it.containsKey("path")) {
-            path = it.path
-        }
-
-        sh """
-        cd ${path} && \
-        ${builder} install && \
-        ${builder} run ${config.b_config.project.buildCommand ? config.b_config.project.buildCommand : 'build'}
-        """
-    }
+    sh """
+    ${builder} install && \
+    ${builder} run ${config.b_config.project.buildCommand ? config.b_config.project.buildCommand : 'build'}
+    """
 }
