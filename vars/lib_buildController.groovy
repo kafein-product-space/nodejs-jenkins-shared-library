96880650594c92250fb39795e38c9a59f7203804
Ferhat Kacar <ferhatkacar.partner@kafein.com.tr>
2024-08-07 11:21:50 +0300

    Updated build controller

--- vars/lib_buildController.groovy
+++ vars/lib_buildController.groovy
@@ -2,18 +2,20 @@ def call(Map config) {
     def builder = "npm"
 
     if (config.b_config.project.builderVersion != "nodejs") {
-        builder = "${tool config.b_config.project.builderVersion}/bin/npm"
+        builder = "${tool name: config.b_config.project.builderVersion}/bin/npm"
     }
 
-    def path = "."
-    if (config.b_config.project.containsKey("path")) {
-        path = config.b_config.project.containsKey("path")
-    }
+    config.b_config.project.each { it ->
+        def path = "."
 
-    sh """
-    cd ${path} && \
-    ${builder} install && \
-    ${builder} run ${config.b_config.project.buildCommand ? config.b_config.project.buildCommand : 'build'}
-    """
+        if (it.containsKey("path")) {
+            path = it.path
+        }
+
+        sh """
+        cd ${path} && \
+        ${builder} install && \
+        ${builder} run ${config.b_config.project.buildCommand ? config.b_config.project.buildCommand : 'build'}
+        """
+    }
 }
-
