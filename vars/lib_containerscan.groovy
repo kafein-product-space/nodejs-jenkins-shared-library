32b0e0d9cd4f10a50239284e12cfaa3e48bde633
Ferhat Kacar <ferhatkacar.partner@kafein.com.tr>
2024-09-07 15:54:18 +0300

    updated post

--- vars/lib_containerscan.groovy
+++ vars/lib_containerscan.groovy
@@ -1,6 +1,6 @@
 // vars/lib_containerscan.groovy
 
-def trivyScan(String imageName, String outputDir = 'trivy-reports') {
+def trivyScan(String imageName, String outputDir = 'trivy-reports', String templatePath = '/home/jenkins/.templates/html.tpl') {
     script {
         try {
             echo "Running Trivy scan for image: ${imageName}"
@@ -11,16 +11,19 @@ def trivyScan(String imageName, String outputDir = 'trivy-reports') {
             // Ensure the output directory exists
             sh "mkdir -p ${outputDir}"
 
-            // Generate HTML report
+            // Generate HTML report using the custom template
             sh """
             docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
-                -v \$(pwd)/${outputDir}:/root/.cache/ aquasec/trivy:latest image \
-                --no-progress --exit-code 1 --format html \
+                -v \$(pwd)/${outputDir}:/root/.cache/ \
+                -v \$(pwd)/${templatePath}:/html.tpl \
+                aquasec/trivy:latest image \
+                --no-progress --exit-code 1 --format template \
+                --template "@html.tpl" \
                 --output ${outputDir}/trivy-report-${imageName.replaceAll('/', '_')}.html \
                 ${imageName}
             """
             
-            echo "Trivy scan completed for image: ${imageName}. Reports available in ${outputDir}."
+            echo "Trivy scan completed for image: ${imageName}. HTML report available in ${outputDir}."
 
         } catch (Exception e) {
             error "Trivy scan failed: ${e.message}"
