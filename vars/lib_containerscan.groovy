ae00587ee60241502e760f5ea41c0aa702ba73f1
Ferhat Kacar <ferhatkacar.partner@kafein.com.tr>
2024-09-07 15:45:25 +0300

    updated post

--- vars/lib_containerscan.groovy
+++ vars/lib_containerscan.groovy
@@ -7,14 +7,14 @@ def trivyScan(String imageName, String outputDir = 'trivy-reports') {
 
             // Pull Trivy and scan the image
             sh "docker pull aquasec/trivy:latest"
-            
+
             // Ensure the output directory exists
             sh "mkdir -p ${outputDir}"
 
             // Generate HTML report
             sh """
             docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
-                -v $(pwd)/${outputDir}:/root/.cache/ aquasec/trivy:latest image \
+                -v \$(pwd)/${outputDir}:/root/.cache/ aquasec/trivy:latest image \
                 --no-progress --exit-code 1 --format html \
                 --output ${outputDir}/trivy-report-${imageName.replaceAll('/', '_')}.html \
                 ${imageName}
